18f256d02ce14dede4d0dabfe11ef54b8f89e7ac
Mikhail Riabov <notryaba@gmail.com>
2025-09-19 17:37:00 +0500

    Add validate dataset

--- dataset.py
+++ dataset.py
@@ -1,10 +1,11 @@
-from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader
 import torchvision.transforms.v2 as v2
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
 
 BATCH_SIZE = 16
 
 transforms = v2.Compose([
+    v2.Grayscale(),
     v2.ToTensor(),
     v2.Resize(size=(28, 28))
 ])
@@ -12,5 +13,11 @@ transforms = v2.Compose([
 train_dataset = MNIST(root="mnist/train", download=False, train=True, transform=transforms)
 test_dataset = MNIST(root="mnist/test", download=False, train=False, transform=transforms)
 
+train_size = int(0.8 * len(train_dataset))
+val_size = len(train_dataset) - train_size
+
+train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+
 train_loader = DataLoader(train_dataset, shuffle=True, batch_size=BATCH_SIZE)
+val_loader = DataLoader(val_dataset, shuffle=True, batch_size=BATCH_SIZE)
 test_loader = DataLoader(test_dataset, shuffle=False, batch_size=BATCH_SIZE)
