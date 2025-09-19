e8c23bf10291a57718f99ad3fa81fb4aa2ecd4ad
Mikhail Riabov <notryaba@gmail.com>
2025-09-19 18:47:30 +0500

    Add batchnorm, maxpool, fewer parameters

--- model.py
+++ model.py
@@ -1,25 +1,30 @@
-import torch
 import torch.nn as nn
+
 
 class MNISTModel(nn.Module):
     def __init__(self, in_channels, out):
         super().__init__()
-        
+
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, 16, kernel_size=3), # 26x26
+            nn.Conv2d(in_channels, 16, kernel_size=3),
+            nn.BatchNorm2d(16), # 26x26
             nn.ReLU(),
             nn.Conv2d(16, 32, kernel_size=3), # 24x24
-            nn.ReLU()
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2) # 12x12
         )
         self.flatten = nn.Flatten()
         self.linear = nn.Sequential(
-            nn.Linear(32*24*24, 1024),
+            nn.Linear(32*12*12, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Linear(1024, 512),
+            nn.Linear(512, 128),
+            nn.BatchNorm1d(128),
             nn.ReLU(),
-            nn.Linear(512, out)
+            nn.Linear(128, out)
         )
-    
+
     def forward(self, x):
         x = self.conv(x)
         x = self.flatten(x)
